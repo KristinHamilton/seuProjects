6d6c934e35e0793068a995a2f4fb040634c2cadd
Kristin Hamilton <kristinhamilton050@gmail.com>
2016-05-09 16:23:28 -0500

    Add index.php

---
+++ cosc2328/index.php
@@ -0,0 +1,99 @@
+<?php
+echo "
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'
+'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml' lang='en' xml:lang='en'>
+	<head>
+		<meta http-equiv='Content-type' content='text/html;charset=UTF-8' />
+		<link rel='stylesheet' href='asg2/start.css' type='text/css' />
+		<title>  Kristin Hamilton | COSC2328 </title>
+	</head>
+	
+	<body>	
+		<div class='heading'>
+			<h2> Kristin Hamilton - COSC2328 Home Page </h2>
+		</div>
+	
+		<!-- start content -->
+		<div class='content'>
+			<div class='floatleft'>
+	
+				<h3> Degree in progress </h3>
+				B.S. Computer Science (May 2017)
+			
+				<h4> _ is/are my favorite! </h4>
+				<ul>
+					<li> Exploring in the woods </li>
+					<li> Playing with the dogs </li>
+					<li> Texas Master Naturalist activities</li>
+					<li> Snowskiing </li>
+					<li> Programming </li>
+					<li> Learning new things </li>
+					<li> Cooking </li>
+					<li> Spending time with family </li>
+					<li> Field guides </li>
+					<li> Bright indoor lighting </li>
+					<li> Texas BBQ and Tex-Mex </li>
+				</ul>
+			
+				<h4> COSC 2328 Assignments (Fall 2013)</h4>
+				<ul>
+					<li>Assignment #2 (current page) CS home page using PHP</li>
+					<li><a href='./asg3.php'>Assignment #3</a> Gas Milage Calculator</li>
+					<li><a href='./asg4.php'>Assignment #4</a> 'Blog' with Commenting System</li>
+					<li><a href='./asg5.php'>Assignment #5</a> Mad Libs</li>
+					<li><a href='./asg6.php'>Assignment #6</a> PHP GD Graphics Library</li>
+					<li><a href='./asg7.php'>Assignment #7</a> Image Database</li>
+					<li><a href='./asg8.php'>Assignment #8</a> Image Database with Login</li>
+					<li><a href='./asg9.php'>Assignment #9</a> Donuts-R-Us Online Ordering</li>
+				</ul>
+			
+				<h6> Some helpful links: </h6>
+			    <a href='http://www.ascii.cl/htmlcodes.htm'> ascii HTML codes - characters and symbols </a>
+		  	    <br /><br />
+		  	    <a href='http://validator.w3.org'> HTML validator </a>
+		  	    <br /><br />
+		  	    <a href='http://jigsaw.w3.org/css-validator/check?uri=referer'>CSS validator</a>
+			    <br /><br />
+		  	    <a href='http://www.ficml.org/jemimap/style/color/wheel.html?aaa=%2300ff00'> color wheel </a>
+		  	    <br /><br />
+		  	    <a href='http://www.ajaxgoals.com/'> ajax resource </a>
+		  	    <br /><br />
+	
+			</div>		
+		
+			<div class='floatleft'>
+				<div class='border'>	
+					<br />
+					<em>Bowie, my dog-nephew, and Charlie (R.I.P.)</em>
+					<br />
+					<img src='BowieAndCharlie.jpg' alt='Bowie and Charlie' />
+				</div>
+	 		</div>
+	
+		</div>
+		<!--  end content  -->
+	
+		<div class='clear'></div>
+	
+		<!-- start footer -->
+		<div class='footer'>
+			St. Edward&apos;s University .:. cosc 2328 .:. fall 2013
+			<br /><br />
+			
+			<em>Last Updated:</em>
+			<script type='text/javascript'>
+				document.write(document.lastModified.slice(0,10));
+			</script>
+		
+			&nbsp;&nbsp;&nbsp;
+			<a href='http://validator.w3.org/check?uri=referer'><strong>HTML</strong></a>
+			&nbsp;&nbsp;&nbsp;
+			<a href='http://jigsaw.w3.org/css-validator/check?uri=referer'><strong>CSS</strong></a>
+			&nbsp;&nbsp;&nbsp;&copy;
+			<a href='http://www.stedwards.edu'><strong>SEU</strong></a>
+		</div>
+		<!--  end footer  -->
+		
+	</body>
+</html>";
