4f0be5c7dacd4effbc25427ade285158ddb48520
Kristin Hamilton <kristinhamilton050@gmail.com>
2016-05-11 12:29:14 -0500

    [asg9.php] Modify formatting (echo HTML statements)

--- cosc2328/asg9/asg9.php
+++ cosc2328/asg9/asg9.php
@@ -1,5 +1,4 @@
 <?php
-
 /* *
  * [asg9.php]
  * main script for asg9
@@ -14,97 +13,67 @@
  *  Donuts-R-Us, online order forms and using js with AJAX to change page content
  *  without reloading page (asynchronous, the "A" in AJAX)
  */
-
 //echo phpinfo();
-//ini_set ("display_errors", "1");
-//error_reporting(E_ALL);
+ini_set ("display_errors", "1");
+error_reporting(E_ALL);
 
-require "asg8funx.php";
-#require for header and footer
-
+require_once "../asg8/asg8funx.php";
 printDocHeadingJS("asg9.css", "Donuts-R-Us", "asg9.js");
-#prints document header with text/javascript included in meta tag
 
-print "<body>\n";
-
-print "<div class='heading'><h1>Donuts-R-Us</h1></div>\n";
-
-print "<div class='back2CShome'><a href='http://cs.stedwards.edu/~khamilt2/cosc2328'>" .
-"Back to CS Home</a></div>\n";
-
-print
-"<!--  start of content  -->\n" .
-"<div class='content'>\n";
-
-
-print
- /* mainMsgDiv */
-"<div id='mainMsgDiv' class='headingcontent' style='width:90%;margin:1.5%;padding:1% 5% 1% 5%;'>".
-"  <h2>Welcome to Donuts-R-Us!</h2>".
- /* close mainMsgDiv */
-"</div>\n";
-
-
-print
- /* errorDiv */
-"<div id='errorDiv' class='errortext' style='width:90%;margin:1.5%;padding:0 5% 0 5%;'>".
- /* close errorDiv */
-"</div>\n";
-
-
-print
- /* bodyDiv */
-"<!--  start of bodyDiv  -->\n".
-"<div id='bodyDiv' style='width:90%;margin:0 1.5%;padding:1% 5% 1% 5%;'>\n";
-
-print
-"<p class='statement'>Please enter your name and your 10-digit phone number to retrieve your most recent order information</p>\n".
-
-"<p>Name:\t<input type='text' id='customerName' size='20' maxlength='30' /></p>\n".
-
-"<p>Phone number:\t<input type='text' id='phoneNumber' size='10' maxlength='10' /></p>\n";
-
-print
- /* close bodyDiv */
-"</div>\n".
-"<!--  end of bodyDiv  -->\n";
-
-
-print
- /* buttonsDiv */
-"<!--  start of buttonsDiv  -->\n".
-"<div id='buttonsDiv'>\n".
-
-   /* buttonDiv1 */
-"  <div id='buttonDiv1' style='width:25%;margin:1.5%;padding:2% 2% 5% 5%;float:left;'>\n".
-   /* close buttonDiv1 */
-"  </div>\n".
-
-   /* buttonDiv2 */
-"  <div id='buttonDiv2' style='width:25%;margin:1.5% auto;padding:2% 2% 5% 5%;float:left;'>\n".
-   /* close buttonDiv2*/
-"  </div>\n".
-
-   /* buttonDiv3 */
-"  <div id='buttonDiv3' style='width:25%;margin:1.5% 0 1.5% 1.5%;padding:2% 0 5% 5%;float:left;'>\n".
-"    <button onclick='submitPhoneForm();' type='button'>Submit</button>\n".
-   /* close buttonDiv3 */
-"  </div>\n".
-
- /* close buttonsDiv */
-"</div>\n".
-"<!--  end of buttonsDiv  -->\n";
-
-
-print
-"</div>\n" ;
-"<!--  end of content  -->\n";
+echo "<body>\n
+		
+<div class='heading'>
+	<h1>Donuts-R-Us</h1>
+</div>\n
+		
+<div class='back2CShome'>
+    <a href='http://kristin.create.stedwards.edu/cosc2328/index.php'>Back to CS Home</a>
+</div>\n
+		
+<!--  start content  -->\n
+<div class='content'>\n
+		
+	<div id='mainMsgDiv' class='headingcontent mainMsgDiv'>
+		<h2>Welcome to Donuts-R-Us!</h2>
+	</div>\n
+		
+	<div id='errorDiv' class='errortext errorDiv'>
+	</div>\n
+		
+	<div id='bodyDiv' class='bodyDiv'>\n
+		<p class='statement'>\n
+			Please enter your name and your 10-digit phone number to retrieve 
+			your most recent order information\n
+		</p>\n
+		<p>Name:\t\n
+			<input type='text' id='customerName' size='20' maxlength='30' />\n
+		</p>\n
+		<p>Phone number:\t\n
+			<input type='text' id='phoneNumber' size='10' maxlength='10' />\n
+		</p>\n
+	</div>\n
+		
+	<div id='buttonsDiv'>\n
+		
+		<div id='buttonDiv1' class='buttonDiv buttonDiv1'>\n
+		</div>\n
+		
+	  	<div id='buttonDiv2' class='buttonDiv buttonDiv2'>\n
+	  	</div>\n
+		
+	  	<div id='buttonDiv3' class='buttonDiv buttonDiv3'>\n
+	    	<button onclick='submitPhoneForm();' type='button'>Submit</button>\n
+	  	</div>\n
+		
+	</div>\n
+		
+</div>\n
+<!--  end content  -->\n";
 
 printDocFooter();
-#prints document footer
 
-print
-"</body>\n".
-"</html>\n";
+echo
+"</body>\n
+</html>\n";
 
 ?>
